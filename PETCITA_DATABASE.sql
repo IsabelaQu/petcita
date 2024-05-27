0dd6454c2c10dc063c2411c6036a8f7443e9b682
Giovanni Mikhael Gonzalez Bergamo <69176031+gMBergamo@users.noreply.github.com>
2024-05-26 21:36:25 -0300

    Alteração de refatoração em PETCITA_DATABASE.sql

--- PETCITA_DATABASE.sql
+++ PETCITA_DATABASE.sql
@@ -7,19 +7,20 @@ CREATE TABLE catalogo(
 	disponivel boolean NOT NULL,
 	valor decimal NOT NULL,
 	nome varchar(100) NOT NULL,
-	descrição varchar(255) NOT NULL,
+	descricao varchar(255) NOT NULL,
 	categoria varchar(30) NOT NULL,
-    PRIMARY KEY (id_catalogo)
+    	PRIMARY KEY (id_catalogo)
 );
 
 CREATE TABLE cat_servico (
 	id_cat_servico int auto_increment NOT NULL,
-	min_duração int NOT NULL,
-	serviço_interno boolean NOT NULL,
-    id_catalogo int NOT NULL, 
+	min_duracao int NOT NULL,
+	servico_interno boolean NOT NULL,
+    	id_catalogo int NOT NULL, 
 	PRIMARY KEY (id_cat_servico),
-    FOREIGN KEY (id_catalogo) REFERENCES catalogo(id_catalogo)
+    	FOREIGN KEY (id_catalogo) REFERENCES catalogo(id_catalogo)
 );
+
 
 CREATE TABLE cat_produto (
 	id_cat_produto int auto_increment NOT NULL,
@@ -27,8 +28,8 @@ CREATE TABLE cat_produto (
 	dt_validade date NOT NULL,
 	dt_registro date NOT NULL,
 	id_catalogo int NOT NULL, 
-    PRIMARY KEY (id_cat_produto),
-    FOREIGN KEY (id_catalogo) REFERENCES catalogo(id_catalogo)
+    	PRIMARY KEY (id_cat_produto),
+    	FOREIGN KEY (id_catalogo) REFERENCES catalogo(id_catalogo)
 );
 
 CREATE TABLE usuario (
@@ -38,10 +39,13 @@ CREATE TABLE usuario (
 	senha varchar(255) NOT NULL,
 	login varchar(30) NOT NULL,
 	email varchar(255) NOT NULL,
-    PRIMARY KEY (id_usuario)
+    	cep varchar(8) NOT NULL,
+	numero_residencia int NOT NULL,
+    	funcionario boolean,
+    	PRIMARY KEY (id_usuario)
 );
 
-CREATE TABLE funcionario (
+/*CREATE TABLE funcionario (
 	id_funcionario int auto_increment NOT NULL,
 	cargo varchar(30) NOT NULL,
     salario decimal NOT NULL,
@@ -70,17 +74,29 @@ CREATE TABLE animal (
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
 
-CREATE TABLE pedido (
-	id_pedido int auto_increment NOT NULL,
-	id_funcionario int NOT NULL,
-	id_cliente int NOT NULL,
-	finalizado boolean NOT NULL,
-    PRIMARY KEY (id_pedido),
-    FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario),
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+*/
+
+CREATE TABLE animal (
+	id_animal int auto_increment NOT NULL,
+	nome varchar(50) NOT NULL,
+	especie varchar(50) NOT NULL,
+	data_nascimento date NOT NULL,
+	porte varchar(1) NOT NULL,
+	id_usuario int NOT NULL,
+    	PRIMARY KEY (id_animal),
+    	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
-CREATE TABLE pedido_itens (
+/*
+CREATE TABLE pedido (
+	id_pedido int auto_increment NOT NULL,
+	id_usuario int NOT NULL,
+	finalizado boolean NOT NULL,
+    PRIMARY KEY (id_pedido),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE pedido_iten (
 	id_pedido_itens int auto_increment NOT NULL,
 	id_cat_produto int NOT NULL,
 	id_pedido int NOT NULL,
@@ -91,7 +107,7 @@ CREATE TABLE pedido_itens (
     FOREIGN KEY (id_cat_produto) REFERENCES cat_produto(id_cat_produto)
 );
 
-CREATE TABLE pedido_agendamentos (
+CREATE TABLE pedido_agendamento (
 	id_pedido_agendamentos int auto_increment NOT NULL,
 	id_pedido int NOT NULL,
     id_cat_servico int NOT NULL,
@@ -99,6 +115,30 @@ CREATE TABLE pedido_agendamentos (
 	data_agendamento date NOT NULL,
     PRIMARY KEY (id_pedido_agendamentos),
     FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
-    FOREIGN KEY (id_cat_servico) REFERENCES servico(id_cat_servico),
+    FOREIGN KEY (id_cat_servico) REFERENCES cat_servico(id_cat_servico),
+	FOREIGN KEY (id_animal) REFERENCES animal(id_animal)
+);
+*/
+
+CREATE TABLE pedido_item (
+	id_pedido_item int auto_increment NOT NULL,
+	id_cat_produto int NOT NULL,
+	quantidade int NOT NULL,
+	data_pedido date NOT NULL,
+	id_usuario int NOT NULL,
+	PRIMARY KEY (id_pedido_itens),
+	FOREIGN KEY (id_cat_produto) REFERENCES cat_produto(id_cat_produto),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE pedido_agendamento (
+	id_pedido_agendamento int auto_increment NOT NULL,
+    	id_cat_servico int NOT NULL,
+    	id_animal int NOT NULL,
+	data_agendamento date NOT NULL,
+    	id_usuario int NOT NULL,
+    	PRIMARY KEY (id_pedido_agendamentos),
+   	FOREIGN KEY (id_cat_servico) REFERENCES cat_servico(id_cat_servico),
+    	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
 	FOREIGN KEY (id_animal) REFERENCES animal(id_animal)
 );
